4842d8a1aca9f99e6b97048d79160f6d7a67e6c9
DuhanMo <devduhanmo@gmail.com>
2021-03-09 17:33:54 +0900

    first project commit

---
+++ front/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <div>
+        <button className="btn btn-success active">로그인</button>
+        <button className="btn btn-secondary active">회원가입</button>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
---
+++ front/src/pages/BoardListPage.js
@@ -0,0 +1,30 @@
+import React, { useEffect } from "react";
+
+const BoardListPage = () => {
+  useEffect(() => {});
+
+  return (
+    <div>
+      <table className="table table-horizontal table-bordered">
+        <thead class="thead-strong">
+          <tr>
+            <th>게시글번호</th>
+            <th>제목</th>
+            <th>작성자</th>
+            <th>최종수정일</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default BoardListPage;
